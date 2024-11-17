83f2db3d6b7f83da30a7109d3d5af9d3121e7208
Shengtao Yao <sy3535@nyu.edu>
2024-11-17 15:02:01 -0500

    aaa

--- set.sh
+++ set.sh
@@ -1,5 +1,7 @@
 generate new post
-pname="haha"
+
+# 不能有空格！
+pname="3rd_yr_fall"
 postdir=./source/_posts
 
 hexo new post ${pname}
@@ -10,4 +12,4 @@ mv ${postdir}/${pname}.md ${postdir}/${pname}/${pname}.md
 
 # hexo new page archives
 # hexo new page tags
-hexo new page links
+# hexo new page links
