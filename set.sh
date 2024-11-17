d326fa6a47afe7a33859531d402aec965295d11f
Shengtao Yao <sy3535@nyu.edu>
2024-11-17 12:13:09 -0500

    aaa

--- set.sh
+++ set.sh
@@ -1,2 +1,9 @@
-# hexo new post "hello world"
-hexo new page articles
+# generate new post
+pname="haha"
+postdir=./source/_posts
+
+hexo new post ${pname}
+
+mkdir ${postdir}/${pname}
+mv ${postdir}/${pname}.md ${postdir}/${pname}/${pname}.md
+# hexo new page articles
