dfe0af1cca9e4a49494b474dcc5f8011c7c11533
Shengtao Yao <sy3535@nyu.edu>
2024-11-17 21:27:52 -0500

    aaa

--- set.sh
+++ set.sh
@@ -1,9 +1,7 @@
-generate new post
+# generate new post
 
 # 不能有空格！
-pname="地球online中的超良性资产"
-hexo new post ${pname}
+# pname="地球online中的超良性资产"
+# hexo new post ${pname}
 
-# hexo new page archives
-# hexo new page tags
-# hexo new page links
+hexo new page academic
---
+++ source/academic/index.md
@@ -0,0 +1,7 @@
+---
+title: academic
+date: 2024-11-17 21:27:17
+---
+
+*施工中。。。*
+*It is currently under construction*
