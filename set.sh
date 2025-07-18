0df74c4b026dd78a65939c87691bbc9c044aaea8
FlappyHimself <flappyyst@outlook.com>
2025-07-18 02:36:49 -0400

    aaa

--- set.sh
+++ set.sh
@@ -1,12 +1,11 @@
 # generate new post
 # 不能有空格！
-
 # name 
-pname="23/24'BLG给我的一场梦9"
+pname="不要他妈的写javascript！"
 hexo new post ${pname}
 
-mkdir /home/flappy/my-blog/source/_posts/${pname}
-mv /home/flappy/my-blog/source/_posts/${pname}.md /home/flappy/my-blog/source/_posts/${pname}/${pname}.md 
+mkdir source/_posts/${pname}
+mv source/_posts/${pname}.md source/_posts/${pname}/${pname}.md 
 
 # flow: 
 # 在post里生成同名文件夹，中间加入图片。 
--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ git add .
 git commit -m "aaa"
 git push 
 
-cd /home/flappy/my-blog/
+cd /mnt/c/Users/flapp/prog/blog
 git submodule update --remote
 hexo clean
 hexo generate
 git add . 
 git commit -m "aaa"
-git push upstream
+git push origin
